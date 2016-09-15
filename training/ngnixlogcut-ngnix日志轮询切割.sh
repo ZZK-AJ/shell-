d95da8e9c4435a89351dd61f0bb7e35e273fb2c6
zzk <1129419956@qq.com>
2016-09-15 18:39:32 +0800

    1

--- training/ngnixlogcut-ngnix日志轮询切割.sh
+++ training/ngnixlogcut-ngnix日志轮询切割.sh
@@ -6,8 +6,8 @@ Datef=`date +%Y%m%d`  #执行date命令，+%Y%m%d 指定日期格式
 Basedir="/applicaton/ngnix"
 ngnixlogdir="/$Basedir/logs"
 logname="access_www"
-[ -d $ngnixlogdir ] && cd $ngnixlogdir||exit 1
-[ -f ${logname}.log ]||exit 1
+[ -d $ngnixlogdir ] && cd $ngnixlogdir||exit 1  #判断日志目录是否存在，存在就cd到对应目录
+[ -f ${logname}.log ]||exit 1                   #判断日志是否存在
 /bin/mv ${logname}.log ${Datef}_${logname}.log
 $Basedir/sbin/ngnix -s reload
 
