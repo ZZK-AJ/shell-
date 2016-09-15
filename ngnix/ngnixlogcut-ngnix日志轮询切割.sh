4520e12cc7c246b947c5513bd23ef80f659cdf55
zzk <1129419956@qq.com>
2016-09-15 18:41:35 +0800

    1

--- ngnix/ngnixlogcut-ngnix日志轮询切割.sh
+++ ngnix/ngnixlogcut-ngnix日志轮询切割.sh
@@ -8,7 +8,7 @@ ngnixlogdir="/$Basedir/logs"
 logname="access_www"
 [ -d $ngnixlogdir ] && cd $ngnixlogdir||exit 1  #判断日志目录是否存在，存在就cd到对应目录
 [ -f ${logname}.log ]||exit 1                   #判断日志是否存在
-/bin/mv ${logname}.log ${Datef}_${logname}.log
-$Basedir/sbin/ngnix -s reload
+/bin/mv ${logname}.log ${Datef}_${logname}.log  #只需要使用mv命令，更改文件名，然后就重启ngnix，会自动生成默认文件名的日志文件
+$Basedir/sbin/ngnix -s reload 
 
 
