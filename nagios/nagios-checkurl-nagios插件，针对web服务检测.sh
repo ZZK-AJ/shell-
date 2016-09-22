3ab3aac8a8dff1b50c704ad8814ab49e221a4736
zzk <1129419956@qq.com>
2016-09-22 23:14:59 +0800

    1

--- nagios/nagios-checkurl-nagios插件，针对web服务检测.sh
+++ nagios/nagios-checkurl-nagios插件，针对web服务检测.sh
@@ -12,10 +12,10 @@ usage()	{
 wget -T 10 --spider $1 >/dev/null 2>&1		#wget进行检测
 if [ $? -eq 0 ];then		#返回值等于0，就说明没有问题
 	echo "URL $1 IS OK"
-	exit 0
+	exit 0             #退出状态码为0，OK
 else
 	echo "URL $1 IS WARNNING"
-	exit 2
+	exit 2             #退出状态码为2，警告状态
 fi
 
 
