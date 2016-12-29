d8b492d9f23d6498f72e21fe00c67963bbc0aed9
zzk <1129419956@qq.com>
2016-12-30 00:07:42 +0800

    1

--- nagios/nagios-checkurl-nagios插件，针对web服务检测.sh
+++ nagios/nagios-checkurl-nagios插件，针对web服务检测.sh
@@ -9,7 +9,8 @@ usage()	{
 	exit 1
 }
 [ $# -ne 1 ]&&usage		#参数不为1就打印帮助
-wget -T 10 --spider $1 >/dev/null 2>&1		#wget进行检测
+wget -T 10 --spider $1 >/dev/null 2>&1		
+# wget进行检测  -T, –timeout=SECONDS 设定响应超时的秒数 使用wget –spider测试下载链接是否有效
 if [ $? -eq 0 ];then		#返回值等于0，就说明没有问题
 	echo "URL $1 IS OK"
 	exit 0             #退出状态码为0，OK
